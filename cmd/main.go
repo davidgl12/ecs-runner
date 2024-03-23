09f41020868a3f3eb820b17bb021c0fc6f593cd0
davidgl12 <david.garcia.leon.12@gmail.com>
2024-03-22 21:32:58 -0600

    le pusimos cosas

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
+	"ecs-runner/internal/model"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
@@ -21,6 +24,12 @@ func main() {
 
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	p := tea.NewProgram(model.GetInitialModel(list.ClusterArns))
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
 	}
 
 	fmt.Println(list.ClusterArns)
